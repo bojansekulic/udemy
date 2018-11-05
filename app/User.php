383296c0ccc7b7739c51a718f95bc5320507583e
Bojan Sekulić <bojan@netpp.local>
2018-11-05 12:55:07 +0100

    Generate Question model

--- app/User.php
+++ app/User.php
@@ -27,4 +27,18 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function questions(){
+
+        return $this->hasMany(Question::class);
+
+    }
+
+    public function setTitleAtribute($value){
+
+
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+
+    }
 }
